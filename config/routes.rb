d946575352ac92a421c3b588bb36af392283abd0
Jerry V <jvr572@gmail.com>
2020-07-07 18:17:10 -0400

    syntax change to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :admins
   resources :programs
   resources :organizations
-  post "login", to: "authentication#login"
+  post "/login", to: "authentication#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
